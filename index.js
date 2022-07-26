45c0dc692d748a23c62c99792959be25c6964ebd
Srivarshan <srivarshan611@gmail.com>
2022-07-26 20:32:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,18 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
+const path = require('path');
 const patientRouter = require('./routes/patientRoute');
 const doctorRouter = require('./routes/doctorRoute');
 const appointmentRouter = require('./routes/appointmentRoute');
 const generalRouter = require('./routes/generalRoute');
 const app = express();
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
